5a3524b0895a94bbdc29d7df29fbb50d9bb11901
guisantos12 <guilherme_2002x@hotmail.com>
2018-03-07 15:21:54 -0400

    Criando a classe posicao

--- XadrexConsole/Program.cs
+++ XadrexConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XadrexConsole.tabuleiro;
 
 namespace XadrexConsole
 {
@@ -10,6 +11,12 @@ namespace XadrexConsole
     {
         static void Main(string[] args)
         {
+            Posicao p;
+
+            p = new Posicao(4, 5);
+
+            Console.WriteLine("Posição: "+p);
+            Console.ReadLine();
         }
     }
 }
---
+++ XadrexConsole/tabuleiro/Posicao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XadrexConsole.tabuleiro
+{
+    class Posicao
+    {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao(int Linha, int Coluna)
+        {
+            this.linha = Linha;
+            this.coluna = Coluna;
+        }
+
+        public override string ToString()
+        {
+            return linha +
+                    ", " +
+                    coluna;
+
+        }
+
+    }
+}
